afba9b3dc9509a46f74b7fb4e64ca42eb0a5648f
wensun163 <wendy.sun2001@gmail.com>
2021-03-09 04:58:29 -0800

    Add files via upload
    Add new updates

--- Likes.java
+++ Likes.java
@@ -3,9 +3,14 @@ package entity;
 public class Likes {
 	private int id;
 	private int userId;
+	private String username; 
+	private int post_id; 
+	private int comment_id; 
 	private int postOrCommentId;
 	private String postOrComment;
 	private String dateLike;
+	private int postLikes;
+	private int commentLikes;
 	
 	public Likes() {}; //end default constructor 
 	
@@ -16,6 +21,12 @@ public class Likes {
 		this.setPostOrCommentId(postOrCommentId);
 		this.setPostOrComment(postOrComment);
 		this.setDateLike(dateLike);
+	}//end constructor 
+
+	public Likes(String username, int post_id, int comment_id) {
+		this.setUsername(username); 
+		this.setPostId(post_id); 
+		this.setCommentId(comment_id);
 	}//end constructor 
 
 	public int getId() {
@@ -57,5 +68,46 @@ public class Likes {
 	public void setDateLike(String dateLike) {
 		this.dateLike = dateLike;
 	}//end setDateLike
+
+	public String getUsername() {
+		return username;
+	}//end getUsername
+
+	public void setUsername(String username) {
+		this.username = username;
+	}//end setUsername
+
+	public int getPostId() {
+		return post_id;
+	}//end getPostId
+
+	public void setPostId(int post_id) {
+		this.post_id = post_id;
+	}//end setPostId
+
+	public int getCommentId() {
+		return comment_id;
+	}//end getCommentId
+
+	public void setCommentId(int commentId) {
+		this.comment_id = commentId;
+	}//end setCommentId
+
+	public int getPostLikes() {
+		
+		return postLikes;
+	}//end getPostLikes
+
+	public void setPostLikes(int postLikes) {
+		this.postLikes = postLikes;
+	}//end setPostLikes
+
+	public int getCommentLikes() {
+		return commentLikes;
+	}//end getCommentLikes
+
+	public void setCommentLikes(int commentLikes) {
+		this.commentLikes = commentLikes;
+	}//setCommentLikes
 
 }//end Likes
